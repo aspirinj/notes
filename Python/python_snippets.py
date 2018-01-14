74327ef5d563b7be2b30f88070a6ede7fec3c661
howard j <h.jiao.a@gmail.com>
2018-01-13 19:54:49 -0500

    mod

---
+++ Python/python_snippets.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# In[]
+'''
+Check the source code of a python function
+'''
+import inspect
+lines = inspect.getsourcelines(function_name)
+print("".join(lines[0]))
+
+
+# In[]
+'''
+Stop poping out warning messages
+'''
+import warnings
+warnings.filterwarnings('ignore')
+
+
